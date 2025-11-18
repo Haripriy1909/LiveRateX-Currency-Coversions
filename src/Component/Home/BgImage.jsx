d1b9e920d5c63f08e5647d6e4f815647ec2abc71
Haripriy1909 <haripriydarji1909@gmail.com>
2025-11-18 11:47:17 +0530

    Update BgImage.jsx

--- src/Component/Home/BgImage.jsx
+++ src/Component/Home/BgImage.jsx
@@ -4,7 +4,7 @@ const BgImage = () => {
   return (
     <div className="absolute  w-screen h-screen">
       <img
-        src="https://static.vecteezy.com/system/resources/previews/019/523/909/non_2x/abstract-currency-digital-finance-technology-modern-currency-exchange-transfer-profit-on-modern-background-futuristic-wave-flowing-blue-vector.jpg"
+        src="https://www.technocrazed.com/wp-content/uploads/2015/12/black-wallpaper-to-set-as-background-28.jpg"
        
         className="w-screen h-screen object-cover"
       />
