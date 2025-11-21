8a5384e877c124e2a1e4308da3847990552adaca
Haripriy1909 <haripriydarji1909@gmail.com>
2025-11-21 11:00:21 +0530

    Refactor BgImage component for better structure
    Refactored BgImage component to simplify structure and improve styling.

--- src/Component/Home/BgImage.jsx
+++ src/Component/Home/BgImage.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const BgImage = () => {
   return (
-    <div className="absolute  w-screen h-screen">
-      <img
-        src="https://www.technocrazed.com/wp-content/uploads/2015/12/black-wallpaper-to-set-as-background-28.jpg"
-       
-        className="w-screen h-screen object-cover"
-      />
-    </div>
+    <img
+      src="https://www.technocrazed.com/wp-content/uploads/2015/12/black-wallpaper-to-set-as-background-28.jpg"
+      className="
+        absolute inset-0 -z-10
+        w-full h-full 
+        object-cover
+      "
+    
+    />
   );
 };
 
